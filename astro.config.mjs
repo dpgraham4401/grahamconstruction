c4d5086a9a2b1584254c610c395cbbf938d584bb
David Graham <dpgraham4401@gmail.com>
2024-09-20 09:32:51 -0400

    project set up
        extend global to include alpinejs, use aplinejs ts plugin in tsconfig
        update alpinejs, prettier, tailwindcss dependencies
        remove unsupported config for viewTransition experimental feature (no longer experimental), add alpinejs collapse types, temporarily remove dark theme button functionality

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,5 @@ import compress from 'astro-compress';
 
 // https://astro.build/config
 export default defineConfig({
-  experimental: {
-    viewTransitions: true,
-  },
   integrations: [tailwind(), compress()],
 });
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,11 @@
+/// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+import { Alpine as AlpineType } from 'alpinejs';
+
+declare global {
+  var Alpine: AlpineType;
+}
+
+interface Window {
+  Alpine: import('alpinejs').Alpine;
+}
