4f77f65ddbeddc93b00c31cf35eabb9d6099a2cc
David Graham <dpgraham4401@gmail.com>
2024-09-26 14:03:46 -0400

    add carousel component and use on home page

---
+++ src/components/home/PicCarousel.tsx
@@ -0,0 +1,40 @@
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from '@/components/ui/Carousel';
+import Autoplay from 'embla-carousel-autoplay';
+import { useRef } from 'react';
+
+export const PicCarousel = () => {
+  const plugin = useRef(Autoplay({ delay: 4000, stopOnMouseEnter: true, playOnInit: true }));
+
+  return (
+    <Carousel
+      plugins={[plugin.current]}
+      className="my-12 w-9/12 max-w-fit"
+      onMouseEnter={plugin.current.stop}
+      onMouseLeave={plugin.current.reset}
+    >
+      <CarouselContent>
+        {Array.from({ length: 5 }).map((_, index) => (
+          <CarouselItem key={index} className="">
+            <div className="p-1">
+              <img
+                className="h-auto w-full rounded-3xl"
+                width="1280"
+                height="640"
+                src="/assets/hero.webp"
+                alt="Hero picture"
+              />
+            </div>
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+      <CarouselPrevious />
+      <CarouselNext />
+    </Carousel>
+  );
+};
