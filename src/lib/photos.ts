f31c9c0997b3bd1eb00a18d449eae268cab82588
David Graham <dpgraham4401@gmail.com>
2025-03-08 12:43:53 -0500

    add remaining images from initial photos

--- src/lib/photos.ts
+++ src/lib/photos.ts
@@ -1,15 +1,25 @@
-import patioLawther from '/assets/graham_co_photos/GR3_0011.jpg?url';
-import patioLawther2 from '/assets/graham_co_photos/GR3_0021.jpg?url';
-import patioLawther3 from '/assets/graham_co_photos/GR3_0031.jpg?url';
+import patioMeadow1 from '/assets/graham_co_photos/GR3_0011.jpg?url';
+import patioMeadow2 from '/assets/graham_co_photos/GR3_0021.jpg?url';
+import patioMeadow3 from '/assets/graham_co_photos/GR3_0031.jpg?url';
 import livingRoomLawther1 from '/assets/graham_co_photos/GR3_0056.jpg?url';
 import officeLawther1 from '/assets/graham_co_photos/GR3_0111.jpg?url';
 import libraryImage from '/assets/graham_co_photos/GR3_1244.jpg?url';
 import dresserCondo1 from '/assets/graham_co_photos/GR3_1306.jpg?url';
+import bathroomMeadows1 from '/assets/graham_co_photos/GR3_1434.jpg?url';
 import garageImage from '/assets/graham_co_photos/GR3_1487.jpg?url';
 import kitchenCondo1 from '/assets/graham_co_photos/GR3_1526.jpg?url';
+import kitchenCondo2 from '/assets/graham_co_photos/GR3_1546.jpg?url';
 import apartmentBedroomImage from '/assets/graham_co_photos/GR3_1639.jpg?url';
-import kitchenDenImage from '/assets/graham_co_photos/GR3_6152.jpg?url';
-import billiardsImage from '/assets/graham_co_photos/GR3_6197.jpg?url';
+import bedroomCondo1 from '/assets/graham_co_photos/GR3_1655.jpg?url';
+import frontHouseHighGate from '/assets/graham_co_photos/GR3_2744.jpg?url';
+import kitchenHighGate from '/assets/graham_co_photos/GR3_2935.jpg?url';
+import kitchenLawther1 from '/assets/graham_co_photos/GR3_6098.jpg?url';
+import fireplaceLawther from '/assets/graham_co_photos/GR3_6127.jpg?url';
+import kitchenDenLawther1 from '/assets/graham_co_photos/GR3_6152.jpg?url';
+import kitchenDenLawther2 from '/assets/graham_co_photos/GR3_6157.jpg?url';
+import kitchenPantryLawther1 from '/assets/graham_co_photos/GR3_6187.jpg?url';
+import billiardsLawther1 from '/assets/graham_co_photos/GR3_6197.jpg?url';
+import backyardHighGate1 from '/assets/graham_co_photos/GR3_9930.jpg?url';
 
 export interface HeroImage {
   src: string;
@@ -26,7 +36,7 @@ export const carouselPhotos: HeroImage[] = [
     alt: 'An over-sized, standalone garage at night with light streaming out of the open doors',
   },
   {
-    src: kitchenDenImage,
+    src: kitchenDenLawther1,
     alt: 'a traditionally styled kitchen with a large island and a cozy den',
   },
   {
@@ -34,7 +44,7 @@ export const carouselPhotos: HeroImage[] = [
     alt: 'a long bedroom with a fireplace, sliding door closets, and a private bathroom',
   },
   {
-    src: billiardsImage,
+    src: billiardsLawther1,
     alt: 'a billiards room with a pool table, bar, and arcade games',
   },
 ];
@@ -49,7 +59,7 @@ export const allGrahamPhotos: HeroImage[] = [
     alt: 'An over-sized, standalone garage at night with light streaming out of the open doors',
   },
   {
-    src: kitchenDenImage,
+    src: kitchenDenLawther1,
     alt: 'a traditionally styled kitchen with a large island and a cozy den',
   },
   {
@@ -57,19 +67,19 @@ export const allGrahamPhotos: HeroImage[] = [
     alt: 'a long bedroom with a fireplace, sliding door closets, and a private bathroom',
   },
   {
-    src: billiardsImage,
+    src: billiardsLawther1,
     alt: 'a billiards room with a pool table, bar, and arcade games',
   },
   {
-    src: patioLawther,
+    src: patioMeadow1,
     alt: 'Back patio viewed towards the house.',
   },
   {
-    src: patioLawther2,
+    src: patioMeadow2,
     alt: 'Back patio viewed towards the house.',
   },
   {
-    src: patioLawther3,
+    src: patioMeadow3,
     alt: 'Back patio viewed towards the house.',
   },
   {
@@ -86,6 +96,46 @@ export const allGrahamPhotos: HeroImage[] = [
   },
   {
     src: kitchenCondo1,
-    alt: 'Back patio viewed towards the house.',
+    alt: 'Long kitchen with bright white cabinets and popping pastel colors.',
+  },
+  {
+    src: kitchenCondo2,
+    alt: 'Long kitchen with bright white cabinets and popping pastel colors number 2.',
+  },
+  {
+    src: bathroomMeadows1,
+    alt: 'White granite bathroom with a large mirror and a glass shower.',
+  },
+  {
+    src: bedroomCondo1,
+    alt: 'Master bedroom with a long series of closets.',
+  },
+  {
+    src: frontHouseHighGate,
+    alt: 'Brick house with a large front yard and a gravel driveway.',
+  },
+  {
+    src: kitchenHighGate,
+    alt: 'Unfurnished kitchen with a island and stove with overhead hood.',
+  },
+  {
+    src: kitchenLawther1,
+    alt: 'Beautiful kitchen with white granite counter top and stain glass lights',
+  },
+  {
+    src: fireplaceLawther,
+    alt: 'Fireplace with a large mirror and a wooden mantle.',
+  },
+  {
+    src: kitchenDenLawther2,
+    alt: 'Kitchen with a large island and a cozy den.',
+  },
+  {
+    src: kitchenPantryLawther1,
+    alt: 'Kitchen with a large pantry and a large island.',
+  },
+  {
+    src: backyardHighGate1,
+    alt: 'Backyard with a pool and a large patio.',
   },
 ];
