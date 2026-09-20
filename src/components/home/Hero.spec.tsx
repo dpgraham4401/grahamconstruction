ce4a17929c7eff348d4915f18e300d36d89bdbd6
David Paul Graham <43794491+dpgraham4401@users.noreply.github.com>
2026-09-19 20:22:02 -0400

    Dependency Review and updates (#81)
    * astro upgrade script
    
    # Conflicts:
    #	package.json
    #	pnpm-lock.yaml
    
    * update wrangler
    
    * update typescript and all tailwindcss dependencies
    
    * update node version to 26
    
    * update playwright testing and types dependencies
    
    * update production dependencies
    
    * fix mock location
    
    * config updates
    
    * update pnpm to latest version
    
    * pnpm install updates

--- src/components/home/Hero.spec.tsx
+++ src/components/home/Hero.spec.tsx
@@ -2,12 +2,12 @@ import { render, screen } from '@testing-library/react';
 import { describe, expect, it, vi } from 'vitest';
 import { Hero } from './Hero';
 
-describe('Hero component', () => {
-  vi.mock('@/components/home/HomeCarousel', async (importOriginal) => ({
-    ...(await importOriginal<typeof import('@/components/home/HomeCarousel')>()),
-    HomeCarousel: () => <div>HomeCarousel</div>,
-  }));
+vi.mock('@/components/home/HomeCarousel', async (importOriginal) => ({
+  ...(await importOriginal<typeof import('@/components/home/HomeCarousel')>()),
+  HomeCarousel: () => <div>HomeCarousel</div>,
+}));
 
+describe('Hero component', () => {
   it('renders the main heading', () => {
     render(<Hero />);
     const heading = screen.getByRole('heading', {
